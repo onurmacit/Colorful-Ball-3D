5a6d6a3a4830c430bb765ca2a171d07a34ff2792
onurmacit <onurmaciit@gmail.com>
2023-04-02 03:28:18 +0300

    movement method optimized and environment colored

--- Colorful Ball 3D/Assets/Scripts/Player.cs
+++ Colorful Ball 3D/Assets/Scripts/Player.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject vectorBack;
+     public GameObject vectorForward;
+    public GameObject cam;
+    public Rigidbody rb;
     private Touch touch;
+    [Range(20,40)]
     public int speedModifier;
+    public int forwardSpeed;
 
+    
     public void Update()
     {
+        if(Variables.firsTouch == 1)
+        {
+           transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+           cam.transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+           vectorBack.transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+           vectorForward.transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+        }
+
+
+
+
+
+
+
+
+
+
         if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);   
-
-            if(touch.phase == TouchPhase.Moved)
+            
+            if(touch.phase == TouchPhase.Began)
             {
-                transform.position = new Vector3(transform.position.x+touch.deltaPosition.x * speedModifier * Time.deltaTime,
-                                                 transform.position.y,
-                                                 transform.position.z + touch.deltaPosition.y * speedModifier * Time.deltaTime);
+                Variables.firsTouch = 1;
             }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+               rb.velocity = new Vector3(touch.deltaPosition.x * speedModifier * Time.deltaTime,
+                                         transform.position.y,
+                                         touch.deltaPosition.y * speedModifier * Time.deltaTime);
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity =  Vector3.zero;
+            }         
         }
     }
 }
