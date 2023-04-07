9260c74f2b5360bef5a6fe8b1947717e0c60219d
onurmacit <onurmaciit@gmail.com>
2023-04-07 03:47:45 +0300

    Add buttons and animations

--- Colorful Ball 3D/Assets/Scripts/UIManager.cs
+++ Colorful Ball 3D/Assets/Scripts/UIManager.cs
@@ -9,6 +9,79 @@ public class UIManager : MonoBehaviour
   public GameObject whiteEffectİmage;
   public Image ımage;
   public float duration;
+
+  public Animator layoutAnimator;
+
+  //Butonlar
+  public GameObject settingsOpen;
+  public GameObject settingsClose; 
+  public GameObject soundOn;
+  public GameObject soundOff;
+  public GameObject vibrationOn;
+  public GameObject vibrationOff;
+  public GameObject iap;
+  public GameObject information;
+
+
+
+  //Buton Fonksiyonları
+
+  public void SettingsOpen(){
+    settingsOpen.SetActive(false);
+    settingsClose.SetActive(true);
+    layoutAnimator.SetTrigger("slide_in");
+  }
+
+  public void SettingsClose(){
+    settingsOpen.SetActive(true);
+    settingsClose.SetActive(false);
+    layoutAnimator.SetTrigger("slide_out");
+  }
+
+  public void SoundOn (){
+    soundOn.SetActive(false);
+    soundOff.SetActive(true);
+  }
+
+  public void SoundOff (){
+    soundOn.SetActive(true);
+    soundOff.SetActive(false);
+  }
+
+  public void VibrationOn (){
+    vibrationOn.SetActive(false);
+    vibrationOff.SetActive(true);
+  }
+
+   public void VibrationOff (){
+    vibrationOn.SetActive(true);
+    vibrationOff.SetActive(false);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   public void Start(){
     whiteEffectİmage.SetActive(false);
